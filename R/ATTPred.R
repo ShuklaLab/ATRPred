6ff1648d3cc0f178db406537e8104ceb6a00d867
Bodhayan Prasad <bodhayan@live.com>
2021-04-19 02:10:23 +0530

    first commit by Bodhayan

---
+++ R/ATTPred.R
@@ -0,0 +1,30 @@
+#' ATRPred: Anti-TNF Treatment Response Predictor
+#'
+#' This function predicts the response to anti-TNF therapy in Rheumatoid Arthritis patients at baseline
+#' It requires an excel input for 17 proteins along with gender and baseline DAS
+#' Any values not present will be imputed
+#' Please don't change the order of proteins in the sample excel file
+#'
+#' @export
+ATRPred <- function(){
+  options(warn=-1, verbose = FALSE) # Comment to not suppress the warnings
+	print("##### ATRPred: Anti-TNF Treatment Response Predictor #####")
+	print("Please select the excel file having pateint's NPX values:")
+	patient <- readxl::read_excel(file.choose())
+	gen=patient[2,2]
+	newRow <- patient[c(1,3:19),2]
+	rownames(newRow)=colnames(spl)
+	consolidated <- rbind(spl,t(newRow))
+
+#	require('RANN')
+	hpc <- caret::preProcess(consolidated, method = c("knnImpute","center", "scale"))
+	transformed <- stats::predict(hpc, newdata = consolidated)
+
+	val <- cbind(gen,transformed[90,])
+	wt <- c(0.116,2.133,-2.126,-2.068,0.421,2.488,-2.595,-0.960,-0.651,2.557,-0.830,-0.758,1.281,0.744,-0.421,2.661,-0.243,2.990,-2.574)
+	S <- sum(wt*val)+3.8
+	t <- 0.883
+	p <- 1/(1+exp(-S+t))
+	print(paste0(sprintf('Calculated probabilty of response = %.2f',100*p),"%"))
+	if(S>t) print('The patient is a RESPONDER.') else print('The patient is a NON-RESPONDER.')
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# ATRPred: Anti-TNF Treatment Response Predictor
+
+A package for predicting whether a Rheumatoid Arthritis patient will respond to anti-TNF treatment.
+
+**Visit us at:** [https://github.com/ShuklaLab/ATRPred](https://github.com/ShuklaLab/ATRPred)
+
+## Installation
+
+To install the latest version of ATRPred, please follow the steps:
+
+```
+install.packages("devtools")
+devtools::install_github("ShuklaLab/ATRPred")
+```
+
+To update the package just run the `install_github("ShuklaLab/ATRPred")` command again.
+
+To run the code; please run the following code (input template along with sample inputs are present in examples folder):
+```
+library("ATRPred")
+ATRPred()
+```
+
+Or directly, by typing:
+
+```
+ATRPred::ATRPred()
+```
